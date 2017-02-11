38c73c783e8b842629cf5ea99f310881dc5babf1
Sundy <sundycoder@gmail.com>
2017-02-11 20:04:45 +0800

    gateWay(vs2013 version).

---
+++ gateWay/main.cpp
@@ -0,0 +1,11 @@
+
+#include <stdlib.h>
+#include <string>
+#include "Serial/inc/SerialClass.h"
+#include "Serial/inc/Robot.h"
+
+int main(){
+	SerialTest(3); //3 is COM3
+	system("pause");
+	return 0;
+}
